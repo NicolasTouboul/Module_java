1a0657f43c6405cb85a66da92b86566c7bbeb28a
Nico <touboulnicolas@gmail.com>
2021-11-09 12:19:19 +0100

    Héritage

---
+++ src/package1/Personnage.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class Personnage {
+    protected String nom;
+    private int pointDeVie;
+    public void rencontrer(){
+        System.out.println("wesh");
+    }
+}
---
+++ src/package2/Magicien.java
@@ -0,0 +1,7 @@
+package package2;
+
+import package1.Personnage;
+
+public class Magicien extends Personnage {
+
+}
