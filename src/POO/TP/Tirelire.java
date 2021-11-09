287106fe7f2add356de9d141ec80d894af7f3ac1
Nico <touboulnicolas@gmail.com>
2021-11-09 11:15:26 +0100

    Tirelire fin

--- src/POO/TP/Tirelire.java
+++ src/POO/TP/Tirelire.java
@@ -1,19 +1,23 @@
 package POO.TP;
 
+import java.util.Scanner;
+
 public class Tirelire {
-    double retrait;
-    double depot;
-    double montant;
-    double budget;
+
+   private double montant;
+
 
     public double getMontant() {
         return montant;
     }
+
+
+
     public void afficher(){
         if(montant==0){
-            System.out.printf("Vous êtes le sans sou");
+            System.out.println("Vous êtes le sans sou");
         }else {
-            System.out.printf("Vous avez "+montant+"€ dans votre tirelire");
+            System.out.println("Vous avez "+montant+"€ dans votre tirelire");
         }
 
     }
@@ -27,17 +31,25 @@ public class Tirelire {
             montant=montant+depot;
         }
     }
+
+
+
     public void vider(){
         montant=0;
     }
+
     public void puiser(double retrait){
-        if (retrait<0 || montant-retrait<0){
-            montant=0;
+        if (retrait < 0) {
+            montant=montant;
+        }else if (montant-retrait<=0){
+            vider();
         }else{
             montant = montant-retrait;
         }
 
+
     }
+
     public double calculerSolde(double budget) {
         if (budget <= 0) {
             return montant;
@@ -45,9 +57,33 @@ public class Tirelire {
             return montant - budget;
         }
     }
+
+    public void departEnVacance(double budget){
+        if(calculerSolde(budget)>0){
+            System.out.printf("Vous êtes assez riche pour partir en vacances ! Il vous restera "+ calculerSolde(budget)+"€ à la rentrée");
+        }else{
+            System.out.printf("Il vous manque "+ (budget-montant) +"€ pour partir en vacances");
+        }
+    }
 }
 class Main_3{
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Tirelire montant =new Tirelire();
+        montant.afficher();
+        montant.afficher();
+        System.out.printf("Montant à déposer: ");
+        montant.remplir(input.nextDouble());
+        montant.secouer();
+        montant.afficher();
+        System.out.printf("Montant à retirer: ");
+        montant.puiser(input.nextDouble());
+        montant.afficher();
+        System.out.printf("Budget vacance: ");
+        double budget = input.nextDouble();
+        montant.departEnVacance(budget);
+
 
     }
 }
